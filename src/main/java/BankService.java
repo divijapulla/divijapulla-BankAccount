a1baa821861528956ab58d0f95dad2c37e95a190
divijapulla <divijapulla>
2025-06-02 09:24:02 +0530

    Completed Lab BankAccount

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -4,13 +4,16 @@
  * as they could be, and it also makes this class capable of being tested with automated tests, since testing
  * a class that receives user input is difficult.
  */
+
 public class BankService {
+
     /**
      * The balance should be kept private. If it were public, then other developers could write code in other classes
      * that could cause the balance to exhibit unintended behavior, like being negative. The private access modifier
      * will allow only the methods within this class to interact with the balance.
      */
     private double balance;
+   
     /**
      * A constructor to build a BankService object that will start the bank balance at 0. There is no need to change
      * anything in this constructor.
@@ -23,6 +26,9 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
+         if (amount > 0) {
+            balance += amount;
+        }
 
     }
 
@@ -32,6 +38,9 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
+           if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
 
     }
 
@@ -40,6 +49,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
